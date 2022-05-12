965dc2b57f033a6e6914a1eaee8da37af45ed0bd
msentesy <103088873+msentesy@users.noreply.github.com>
2022-05-12 00:53:25 -0400

    Update manager.py

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -41,6 +41,7 @@ def manager_init() -> None:
     ("HasAcceptedTerms", "0"),
     ("NoOffroadFix", "0"),
     ("OpenpilotEnabledToggle", "1"),
+    ("LanelessMode", "0"),
   ]
   if not PC:
     default_params.append(("LastUpdateTime", datetime.datetime.utcnow().isoformat().encode('utf8')))
